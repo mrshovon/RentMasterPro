5c54d205f5dfc5019603d8ad9cc2a43f096af34b
Md. Moshfequr Rahman <md.moshfequr@lbsbd.com>
2026-04-27 12:56:57 +0600

    Create firebase-messaging-sw.js for FCM push notifications
    - Added Firebase Cloud Messaging service worker
    - Handles background push notifications
    - Required for FCM token registration

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTyvem4AV1deCh5WzG20NzR0fOBPQ2qjc",
+  authDomain: "rentmasterpro-45672.firebaseapp.com",
+  databaseURL: "https://rentmasterpro-45672-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rentmasterpro-45672",
+  storageBucket: "rentmasterpro-45672.firebasestorage.app",
+  messagingSenderId: "970817206915",
+  appId: "1:970817206915:web:10ffa2182b9f7ac7ef39b6",
+  measurementId: "G-QRF9V9LLLY"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon-192x192.png',
+    data: payload.data
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
